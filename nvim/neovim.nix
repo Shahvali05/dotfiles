5cac5a585262dffcf2919af3b80145d08b6a5f2e
laraeter <git28282@gmail.com>
2024-09-24 17:10:34 +0300

    create nvim foldren

--- nvim/neovim.nix
+++ nvim/neovim.nix
@@ -1,8 +1,7 @@
- { pkgs, config, ... }: {
+{ pkgs, inputs, ... }:
+{
   programs = {
     neovim = {
-
-      package = pkgs.neovim;
       enable = true;
       defaultEditor = true;
       viAlias = true;
@@ -10,27 +9,73 @@
       vimdiffAlias = true;
       withNodeJs = true;
       extraPackages = with pkgs; [
-
+        lua-language-server
+        gopls
+        xclip
+        wl-clipboard
+        luajitPackages.lua-lsp
+        nil
+        rust-analyzer
+        #nodePackages.bash-language-server
+        yaml-language-server
+        pyright
+        marksman
       ];
-      plugins = (with pkgs.vimPlugins; [
-
-
-      ]) ++ (with pkgs.vimPlugins.nvim-treesitter-parsers; [{
-        plugin = pkgs.symlinkJoin {
-          name = "nvim-treesitter";
-          paths = [
-            pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-            pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies
-          ];
-        };
-        optional = false;
-      }]);
-
+      plugins = with pkgs.vimPlugins; [
+        alpha-nvim
+        auto-session
+        bufferline-nvim
+        dressing-nvim
+        indent-blankline-nvim
+        nui-nvim
+        #finecmdline
+        nvim-treesitter.withAllGrammars
+        lualine-nvim
+        nvim-autopairs
+        nvim-web-devicons
+        nvim-cmp
+        nvim-surround
+        nvim-lspconfig
+        cmp-nvim-lsp
+        cmp-buffer
+        luasnip
+        cmp_luasnip
+        friendly-snippets
+        lspkind-nvim
+        comment-nvim
+        nvim-ts-context-commentstring
+        plenary-nvim
+        neodev-nvim
+        luasnip
+        telescope-nvim
+        todo-comments-nvim
+        nvim-tree-lua
+        telescope-fzf-native-nvim
+        vim-tmux-navigator
+      ];
       extraConfig = ''
         set noemoji
+        nnoremap : <cmd>FineCmdline<CR>
       '';
       extraLuaConfig = ''
-                ${builtins.readFile ./options.lua}
+        ${builtins.readFile ./options.lua}
+        ${builtins.readFile ./keymaps.lua}
+        ${builtins.readFile ./plugins/alpha.lua}
+        ${builtins.readFile ./plugins/autopairs.lua}
+        ${builtins.readFile ./plugins/auto-session.lua}
+        ${builtins.readFile ./plugins/comment.lua}
+        ${builtins.readFile ./plugins/cmp.lua}
+        ${builtins.readFile ./plugins/lsp.lua}
+        ${builtins.readFile ./plugins/nvim-tree.lua}
+        ${builtins.readFile ./plugins/telescope.lua}
+        ${builtins.readFile ./plugins/todo-comments.lua}
+        ${builtins.readFile ./plugins/treesitter.lua}
+        ${builtins.readFile ./plugins/fine-cmdline.lua}
+        require("ibl").setup()
+        require("bufferline").setup{}
+        require("lualine").setup({
+          icons_enabled = true,
+        })
       '';
     };
   };
