63d2476a839a441cad54098caa45e92d66ca4b3d
laraeter <git28282@gmail.com>
2024-09-28 01:00:18 +0300

    gg

--- nvim/plugins/codeium.lua
+++ nvim/plugins/codeium.lua
@@ -1,3 +1,5 @@
 -- Настройка Codeium
 vim.g.codeium_enabled = true -- Включает Codeium
 vim.g.codeium_no_map_tab = true -- Отключает автоматическое использование Tab для Codeium
+vim.g.codeium_debug = true
+
