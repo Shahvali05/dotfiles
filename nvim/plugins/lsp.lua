d0e0de9f330d6e8c003715e929a163420d8ec760
laraeter <git28282@gmail.com>
2024-09-27 23:35:09 +0300

    gg

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -8,3 +8,41 @@ require'lspconfig'.yamlls.setup{}
 require'lspconfig'.bashls.setup{}
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.gopls.setup{}
+
+local formatter = require("formatter")
+
+formatter.setup({
+  logging = false,
+  filetype = {
+    python = {
+      -- Используем black для форматирования Python
+      function()
+        return {
+          exe = "black",
+          args = { "--quiet", "-" },
+          stdin = true,
+        }
+      end,
+    },
+    cpp = {
+      -- Используем clang-format для форматирования C++
+      function()
+        return {
+          exe = "clang-format",
+          args = { "--style=file", "-" },
+          stdin = true,
+        }
+      end,
+    },
+    go = {
+      -- Используем gofmt для форматирования Go
+      function()
+        return {
+          exe = "gofmt",
+          args = {},
+          stdin = true,
+        }
+      end,
+    },
+  },
+})
