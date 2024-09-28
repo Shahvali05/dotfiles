e878652433043517ebb15dbcb00574574471d430
laraeter <git28282@gmail.com>
2024-09-28 15:44:18 +0300

    gg

--- nvim/keymaps.lua
+++ nvim/keymaps.lua
@@ -6,10 +6,10 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- window management
 
 -- Перемещение по вкладкам
---keymap.set("n", "<Tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" })  -- Вперёд по вкладкам
---keymap.set("n", "<S-Tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })  -- Назад по вкладкам
---keymap.set("n", "<leader>x", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- Закрывать вкладку
---keymap.set("n", "<leader>o", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- Открывать новую вкладку
+keymap.set("n", "<Tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" })  -- Вперёд по вкладкам
+keymap.set("n", "<S-Tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })  -- Назад по вкладкам
+keymap.set("n", "<leader>x", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- Закрывать вкладку
+keymap.set("n", "<leader>o", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- Открывать новую вкладку
 
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
