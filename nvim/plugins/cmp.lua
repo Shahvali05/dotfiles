d26461115bb06328a9a73c97a9d521adedb89302
laraeter <git28282@gmail.com>
2024-09-28 13:46:05 +0300

    settings for nvim

--- nvim/plugins/cmp.lua
+++ nvim/plugins/cmp.lua
@@ -29,6 +29,7 @@ cmp.setup({
     { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
+    { name = "codeium" }, -- add Codeium source here
   }),
 
   -- configure lspkind for vs-code like pictograms in completion menu
