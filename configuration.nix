44ab08b3f3f4ffa5feac045c3400cfbfcb0d6a40
laraeter <git28282@gmail.com>
2024-09-29 00:10:04 +0300

    gg

--- configuration.nix
+++ configuration.nix
@@ -73,16 +73,18 @@
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # +++++++++++++++++++
+  #services.xserver.desktopManager.gnome.enable = true;
+  # +++++++++++++++++++
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "gb";
+    layout = "us";
     variant = "";
   };
 
   # Configure console keymap
-  console.keyMap = "uk";
+  console.keyMap = "us";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
