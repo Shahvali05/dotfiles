b0fa5309497bb6d0e18166e2d97395d3205d888c
laraeter <git28282@gmail.com>
2024-09-29 23:04:06 +0300

    settings nvim-ufo

--- nvim/plugins/nvim-ufo.lua
+++ nvim/plugins/nvim-ufo.lua
@@ -1,4 +1,13 @@
 require('ufo').setup()
+
+-- Настройка для отображения превью свернутого кода
+vim.keymap.set('n', 'K', function()
+  local winid = require('ufo').peekFoldedLinesUnderCursor()
+  if not winid then
+    vim.lsp.buf.hover()
+  end
+end)
+
 --vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
