8a4c235aad012f82ea4e6cc696a8e2f07d30c20e
laraeter <git28282@gmail.com>
2024-09-29 22:55:46 +0300

    gg

--- nvim/neovim.nix
+++ nvim/neovim.nix
@@ -84,6 +84,7 @@
         ${builtins.readFile ./plugins/formatter.lua}
         ${builtins.readFile ./plugins/lsp.lua}
         ${builtins.readFile ./plugins/nvim-tree.lua}
+        ${builtins.readFile ./plugins/nvim-ufo.lua}
         ${builtins.readFile ./plugins/telescope.lua}
         ${builtins.readFile ./plugins/todo-comments.lua}
         ${builtins.readFile ./plugins/treesitter.lua}
--- nvim/options.lua
+++ nvim/options.lua
@@ -23,11 +23,5 @@ vim.g.mapleader = " "
 vim.opt.colorcolumn = "81"
 vim.cmd("highlight ColorColumn ctermbg=red guibg=#232234")
 
-require('ufo').setup()
-vim.o.foldcolumn = '1'
-vim.o.foldlevel = 99
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
-
 --vim.o.foldmethod = 'indent'
 --vim.cmd('autocmd BufReadPost * normal! zR')
