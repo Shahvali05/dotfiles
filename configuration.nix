5a7661f20e9221bedaa4023f317435cd900601dc
laraeter <git28282@gmail.com>
2024-09-29 16:08:55 +0300

    gg

--- configuration.nix
+++ configuration.nix
@@ -64,18 +64,14 @@
     };
   };
 
-  # +++++++++++++++++++
-  #security.pam.services.greetd.enableGnomeKeyring = true;
-  # +++++++++++++++++++
+  security.pam.services.greetd.enableGnomeKeyring = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  # +++++++++++++++++++
-  #services.xserver.desktopManager.gnome.enable = true;
-  # +++++++++++++++++++
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
