57bf5d5f660cedf55f7838cb50477cf99c9e658c
laraeter <git28282@gmail.com>
2024-10-02 20:32:26 +0300

    nvim-dap

--- config/nvim/keymaps.lua
+++ config/nvim/keymaps.lua
@@ -16,3 +16,17 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Nvim-dap
+vim.api.nvim_set_keymap('n', '<F5>', ':lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F10>', ':lua require"dap".step_over()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F11>', ':lua require"dap".step_into()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F12>', ':lua require"dap".step_out()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', ':lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>B', ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>lp', ':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dr', ':lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>dl', ':lua require"dap".run_last()<CR>', { noremap = true, silent = true })
+
+-- Open DAP UI
+vim.api.nvim_set_keymap('n', '<leader>du', ':lua require("dapui").toggle()<CR>', { noremap = true, silent = true })
--- config/nvim/plugins/nvim-dap.lua
+++ config/nvim/plugins/nvim-dap.lua
@@ -1,3 +1,27 @@
-require('dapui').setup()
+local dap = require('dap')
+local dapui = require('dapui')
+local dap_virt_text = require("nvim-dap-virtual-text")
 
+-- nvim-dap-ui setup
+dapui.setup()
+
+-- Virtual text setup
+dap_virt_text.setup()
+
+-- Python adapter
+require('dap-python').setup('~/.virtualenvs/debugpy/bin/python') -- change the path to your python interpreter
+
+-- Go adapter
 require('dap-go').setup()
+
+-- Automatically open and close dap-ui on dap events
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
+
