c10b391ba488469ca4284cb9b6d2f8aa20c5b247
laraeter <git28282@gmail.com>
2024-10-02 18:23:27 +0300

    settings codeium

--- config/nvim/plugins/codeium.lua
+++ config/nvim/plugins/codeium.lua
@@ -1,4 +1,4 @@
-require'lspconfig'.codeium-vim.setup{
+require'lspconfig'.codeium-language-server.setup{
   cmd = { "env", "NIX_LD_x86_64-linux=/nix/store/c10zhkbp6jmyh0xc5kd123ga8yy2p4hk-glibc-2.39-52/lib/ld-linux-x86-64.so.2", "./language_server_linux_x64" },
   -- другие настройки
 }
