d093890dfee26b91dfaad55e37335fd3c36baa63
laraeter <git28282@gmail.com>
2024-10-02 21:32:46 +0300

    nvim-dap

--- configuration.nix
+++ configuration.nix
@@ -177,6 +177,7 @@
     # -------------------------------------
     # system's programs
     # -------------------------------------
+    python312Packages.debugpy
     networkmanagerapplet
     pavucontrol
     dbus
