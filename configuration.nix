a2ec4754497a181f24f198663df16b0ccae51e52
laraeter <git28282@gmail.com>
2024-10-02 18:10:34 +0300

    settings codeium

--- configuration.nix
+++ configuration.nix
@@ -209,7 +209,9 @@
   ];
 
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = [];
+  programs.nix-ld.libraries = [
+    pkgs.glibc
+  ];
 
   # -------------------------------------------------------------------------------------------------------------
   # permittedInsecurePackages
