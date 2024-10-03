1387dea0b1a09a1c939182d419f529ec82e6b274
laraeter <git28282@gmail.com>
2024-10-03 16:03:25 +0300

    treesitter

--- config/nvim/plugins/treesitter.lua
+++ config/nvim/plugins/treesitter.lua
@@ -1,17 +1,9 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "c", "cpp", "go", "python" },  -- Укажите языки, которые хотите установить
+  ensure_installed = {},
 
   auto_install = false,
 
   highlight = { enable = true },
 
   indent = { enable = true },
-
-  parser_dir = "~/.local/share/nvim/treesitter",  -- Указываем пользовательский каталог
-
-  rainbow = {
-    enable = true,
-    extended_mode = true,  -- Включите, если хотите окрашивать больше типов скобок
-    max_file_lines = nil,  -- Не ограничивайте количество строк
-  },
 }
