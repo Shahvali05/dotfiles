940a26d7adfe28519d08dbbe60355db8691e33b8
laraeter <git28282@gmail.com>
2024-10-03 15:26:56 +0300

    nvim-dap

--- config/nvim/keymaps.lua
+++ config/nvim/keymaps.lua
@@ -17,3 +17,59 @@ keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- 
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
+
+-- Определяем таблицу с горячими клавишами
+local keymap_list = {
+  "jk: Выход из режима ввода",
+  "<leader>nh: Очистить подсветку поиска",
+  "<Tab>: Вперёд по вкладкам",
+  "<S-Tab>: Назад по вкладкам",
+  "<leader>x: Закрыть вкладку",
+  "<leader>n: Открыть новую вкладку",
+  "<leader>sv: Разделить окно по вертикали",
+  "<leader>sh: Разделить окно по горизонтали",
+  "<leader>se: Сохранить размер окна",
+  "<leader>sx: Закрыть текущее окно",
+  "<leader>tf: Открыть текущий буфер в новом вкладке",
+  "<leader>wr: Восстановить сеанс для текущего каталога",
+  "<leader>ws: Сохранить сеанс для текущего каталога",
+  "<F5>: Продолжить выполнение программы",
+  "<F10>: Шагнуть через",
+  "<F11>: Шагнуть внутрь",
+  "<F12>: Шагнуть наружу",
+  "<leader>b: Переключить точку останова",
+  "<leader>B: Установить точку останова",
+  "<leader>lp: Установить точку логирования",
+  "<leader>du: Открыть/закрыть DAP UI",
+  "<C-n>: Открыть/закрыть NvimTree",
+  "K: Посмотреть на реализацию",
+  "<leader>ff: Поиск файлов",
+  "<leader>lg: Поиск по содержанию",
+  "<leader>fb: Поиск в буфере",
+  "<leader>fh: Поиск по справочным страницам Neovim",
+  "<leader>ft: Поиск комментариев в TODO",
+  "<leader>]t: Слудующий TODO комент",
+  "<leader>[t: Предыдущий TODO комент",
+  -- Добавьте сюда все свои горячие клавиши
+}
+
+-- Глобальная функция для отображения списка горячих клавиш в правом сплите
+_G.show_keymaps = function()
+  -- Открываем новый сплит справа
+  vim.cmd('vsplit')
+  
+  -- Создаем новый буфер и заполняем его текстом
+  local bufnr = vim.api.nvim_create_buf(false, true) -- Создаем новый пустой буфер
+  vim.api.nvim_set_current_buf(bufnr) -- Активируем этот буфер в текущем окне
+
+  -- Добавляем текст в буфер
+  for _, keymap in ipairs(keymap_list) do
+      vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { keymap })
+  end
+
+  -- Делаем буфер неизменяемым, чтобы случайно не редактировать его
+  vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+end
+
+-- Привязываем функцию к кастомной команде
+vim.api.nvim_set_keymap('n', '<leader>k', ':lua show_keymaps()<CR>', { noremap = true, silent = true })
--- config/nvim/plugins/nvim-dap.lua
+++ config/nvim/plugins/nvim-dap.lua
@@ -33,43 +33,8 @@ vim.api.nvim_set_keymap('n', '<F12>', ':lua require"dap".step_out()<CR>', { nore
 vim.api.nvim_set_keymap('n', '<leader>b', ':lua require"dap".toggle_breakpoint()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>B', ':lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>lp', ':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>dr', ':lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', '<leader>dr', ':lua require"dap".repl.open()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>dl', ':lua require"dap".run_last()<CR>', { noremap = true, silent = true })
 
 -- Open DAP UI
 vim.api.nvim_set_keymap('n', '<leader>du', ':lua require("dapui").toggle()<CR>', { noremap = true, silent = true })
-
-
--- Определяем таблицу с горячими клавишами
-local keymap_list = {
-    "<F5>: Продолжить выполнение программы",
-    "<F10>: Шагнуть через",
-    "<F11>: Шагнуть внутрь",
-    "<F12>: Шагнуть наружу",
-    "<leader>b: Переключить точку останова",
-    "<leader>dr: Открыть REPL",
-    "<leader>du: Открыть/закрыть DAP UI",
-    -- Добавьте сюда все свои горячие клавиши
-}
-
--- Глобальная функция для отображения списка горячих клавиш в правом сплите
-_G.show_keymaps = function()
-    -- Открываем новый сплит справа
-    vim.cmd('vsplit')
-    
-    -- Создаем новый буфер и заполняем его текстом
-    local bufnr = vim.api.nvim_create_buf(false, true) -- Создаем новый пустой буфер
-    vim.api.nvim_set_current_buf(bufnr) -- Активируем этот буфер в текущем окне
-
-    -- Добавляем текст в буфер
-    for _, keymap in ipairs(keymap_list) do
-        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { keymap })
-    end
-
-    -- Делаем буфер неизменяемым, чтобы случайно не редактировать его
-    vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
-end
-
--- Привязываем функцию к кастомной команде
-vim.api.nvim_set_keymap('n', '<leader>k', ':lua show_keymaps()<CR>', { noremap = true, silent = true })
-
