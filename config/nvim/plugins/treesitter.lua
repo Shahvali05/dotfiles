7ffb5c4fe6eae7bf37a80794eda36825ea3f44b2
laraeter <git28282@gmail.com>
2024-10-03 16:38:12 +0300

    treesitter

--- config/nvim/plugins/treesitter.lua
+++ config/nvim/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {},
+  ensure_installed = { "python", "go", "lua", "rust", "typescript", "javascript", "html", "css", "scss", "markdown", "markdown_inline" },
 
   auto_install = false,
 
