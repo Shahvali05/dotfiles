ce0ba2eb62ebb08ea68ffa4f6b5547b7d7bdad91
laraeter <git28282@gmail.com>
2024-10-03 15:50:49 +0300

    treesitter

--- config/nvim/plugins/treesitter.lua
+++ config/nvim/plugins/treesitter.lua
@@ -1,9 +1,11 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {},
+  ensure_installed = { "c", "cpp", "go", "python" },
 
   auto_install = false,
 
   highlight = { enable = true },
 
   indent = { enable = true },
+
+  parser_dir = vim.fn.expand("~/.local/share/nvim/treesitter"), -- Указываем пользовательский каталог
 }
