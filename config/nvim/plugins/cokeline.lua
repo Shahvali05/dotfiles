dc5f0f43b9c12b5953376237e545f73c750d3ad9
laraeter <git28282@gmail.com>
2024-10-04 18:05:45 +0300

    cokeline

--- config/nvim/plugins/cokeline.lua
+++ config/nvim/plugins/cokeline.lua
@@ -2,6 +2,15 @@ require('cokeline').setup({
   rendering = {
     max_buffer_width = 30,
   },
+  default_hl = {
+    fg = function(buffer)
+      return buffer.is_focused and '#ffffff' or '#999999'
+    end,
+    bg = function(buffer)
+      return buffer.is_focused and '#005f87' or '#1c1c1c'
+    end,
+  },
+  fill_hl = 'CokelineFill',  -- Настройка фона для пустых мест
 })
 
 -- Определение серого фона для пустых участков
