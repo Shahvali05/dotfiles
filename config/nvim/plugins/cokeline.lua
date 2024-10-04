d4dfefe6658ea803679180d8f34ec0c72f2e7761
laraeter <git28282@gmail.com>
2024-10-04 18:54:07 +0300

    cokeline

--- config/nvim/plugins/cokeline.lua
+++ config/nvim/plugins/cokeline.lua
@@ -1,24 +1,3 @@
-local api = require('nvim-tree.api')
-
--- Функция для переключения на следующий буфер, не закрывая nvim-tree
-local function switch_to_other_buffer()
-  local current_buf = vim.api.nvim_get_current_buf()
-
-  -- Проверяем, если закрываемый буфер — не nvim-tree, переключаемся на следующий
-  if not api.tree.is_tree_buf(current_buf) then
-    vim.cmd('bnext')  -- Переход на следующий буфер
-    -- Если вдруг переключились на nvim-tree, переключаемся дальше
-    if api.tree.is_tree_buf(vim.api.nvim_get_current_buf()) then
-      vim.cmd('bnext')
-    end
-  end
-end
-
--- Вызов функции при закрытии буфера
-vim.api.nvim_create_autocmd("BufDelete", {
-  callback = switch_to_other_buffer
-})
-
 require('cokeline').setup({
   rendering = {
     max_buffer_width = 30,
