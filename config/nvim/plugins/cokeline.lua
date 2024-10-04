f2bd9c3c342260109845dd415679321f1106f280
laraeter <git28282@gmail.com>
2024-10-04 18:04:49 +0300

    cokeline

--- config/nvim/plugins/cokeline.lua
+++ config/nvim/plugins/cokeline.lua
@@ -2,17 +2,8 @@ require('cokeline').setup({
   rendering = {
     max_buffer_width = 30,
   },
-  default_hl = {
-    fg = function(buffer)
-      return buffer.is_focused and '#ffffff' or '#999999'
-    end,
-    bg = function(buffer)
-      return buffer.is_focused and '#005f87' or '#1c1c1c'
-    end,
-  },
-  fill_hl = 'CokelineFill',  -- Настройка фона для пустых мест
 })
 
 -- Определение серого фона для пустых участков
-vim.api.nvim_set_hl(0, 'CokelineFill', { bg = '#1F1F29' })  -- Серый цвет фона
+vim.api.nvim_set_hl(0, 'CokelineFill', { bg = '#182F40' })  -- Серый цвет фона
 
