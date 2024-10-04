a4a99bb39fd01a5982c85e583c3a8a5f30228855
laraeter <git28282@gmail.com>
2024-10-04 18:18:24 +0300

    nvim config

--- config/nvim/options.lua
+++ config/nvim/options.lua
@@ -5,7 +5,7 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
-opt.wrap = false
+--opt.wrap = false
 opt.ignorecase = true
 opt.smartcase = true
 opt.termguicolors = true
@@ -15,7 +15,7 @@ opt.mouse = "a"
 opt.cursorline = true
 opt.backspace = "indent,eol,start"
 opt.clipboard:append("unnamedplus")
---opt.splitright = true
+opt.splitright = true
 --opt.splitbelow = true
 opt.swapfile = false
 vim.g.mapleader = " "
