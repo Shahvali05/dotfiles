c972af58e150c67e1298456073620280625923fc
laraeter <git28282@gmail.com>
2024-10-05 20:38:01 +0300

    lldb

--- config/nvim/plugins/nvim-dap.lua
+++ config/nvim/plugins/nvim-dap.lua
@@ -14,110 +14,93 @@ require('dap-python').setup('/run/current-system/sw/bin/python3.11') -- change t
 -- Go adapter
 require('dap-go').setup()
 
--- dap.adapters.codelldb = {
-  -- type = 'server',
-  -- port = "${port}",
-  -- executable = {
-    -- command = '/home/laraeter/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/adapter/codelldb',
-    -- args = {"--port", "${port}"},
-  -- }
--- }
-
+-- cpp adapter
 dap.adapters.codelldb = function(on_adapter)
- -- This asks the system for a free port
- local tcp = vim.loop.new_tcp()
- tcp:bind('127.0.0.1', 0)
- local port = tcp:getsockname().port
- tcp:shutdown()
- tcp:close()
+  -- This asks the system for a free port
+  local tcp = vim.loop.new_tcp()
+  tcp:bind('127.0.0.1', 0)
+  local port = tcp:getsockname().port
+  tcp:shutdown()
+  tcp:close()
 
- -- Start codelldb with the port
- local stdout = vim.loop.new_pipe(false)
- local stderr = vim.loop.new_pipe(false)
- local opts = {
-   stdio = {nil, stdout, stderr},
-   args = {'--port', tostring(port)},
- }
- local handle
- local pid_or_err
- handle, pid_or_err = vim.loop.spawn('/home/laraeter/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/adapter/codelldb', opts, function(code)
-   stdout:close()
-   stderr:close()
-   handle:close()
-   if code ~= 0 then
-     print("codelldb exited with code", code)
-   end
- end)
- if not handle then
-   vim.notify("Error running codelldb: " .. tostring(pid_or_err), vim.log.levels.ERROR)
-   stdout:close()
-   stderr:close()
-   return
- end
- vim.notify('codelldb started. pid=' .. pid_or_err)
- stderr:read_start(function(err, chunk)
-   assert(not err, err)
-   if chunk then
-     vim.schedule(function()
-       require("dap.repl").append(chunk)
-     end)
-   end
- end)
- local adapter = {
-   type = 'server',
-   host = '127.0.0.1',
-   port = port
- }
- -- 💀
- -- Wait for codelldb to get ready and start listening before telling nvim-dap to connect
- -- If you get connect errors, try to increase 500 to a higher value, or check the stderr (Open the REPL)
- vim.defer_fn(function() on_adapter(adapter) end, 1000)
+  -- Start codelldb with the port
+  local stdout = vim.loop.new_pipe(false)
+  local stderr = vim.loop.new_pipe(false)
+  local opts = {
+    stdio = {nil, stdout, stderr},
+    args = {'--port', tostring(port)},
+  }
+  local handle
+  local pid_or_err
+  handle, pid_or_err = vim.loop.spawn('/path/to/lldb', opts, function(code)
+    stdout:close()
+    stderr:close()
+    handle:close()
+    if code ~= 0 then
+      print("codelldb exited with code", code)
+    end
+  end)
+  if not handle then
+    vim.notify("Error running codelldb: " .. tostring(pid_or_err), vim.log.levels.ERROR)
+    stdout:close()
+    stderr:close()
+    return
+  end
+  vim.notify('codelldb started. pid=' .. pid_or_err)
+  stderr:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  local adapter = {
+    type = 'server',
+    host = '127.0.0.1',
+    port = port
+  }
+  -- 💀
+  -- Wait for codelldb to get ready and start listening before telling nvim-dap to connect
+  -- If you get connect errors, try to increase 500 to a higher value, or check the stderr (Open the REPL)
+  vim.defer_fn(function() on_adapter(adapter) end, 1000)
 end
 
 -- don't forget to compile/build with debug symbols, otherwise it won't work.
 dap.configurations.cpp = {
- {
-   name = "runit",
-   type = "codelldb",
-   request = "launch",
+  {
+    name = "runit",
+    type = "codelldb",
+    request = "launch",
 
-   program = function()
-     return vim.fn.input('', vim.fn.getcwd() , 'file')
-   end,
+    program = function()
+      -- Добавляем слэш в конце пути, если его нет
+      local cwd = vim.fn.getcwd()
+      if not cwd:match("/$") then
+        cwd = cwd .. "/"
+      end
+      return vim.fn.input('', cwd, 'file')
+      -- return vim.fn.input('', vim.fn.getcwd() , 'file')
+    end,
 
-   args = {"--log_level=all"},
-   cwd = "${workspaceFolder}",
-   stopOnEntry = false,
-   terminal = 'integrated',
-
-   pid = function()
-           local handle = io.popen('pgrep hw$')
-           local result = handle:read()
-           handle:close()
-           return result
-   end
- },
+    args = {"--log_level=all"},
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    terminal = 'integrated',
+ 
+    pid = function()
+            local handle = io.popen('pgrep hw$')
+            local result = handle:read()
+            handle:close()
+            return result
+    end
+  },
 }
 
--- Для других языков можно скопировать и изменить конфигурацию
+-- same for other languages
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.h = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
- 
-
--- dap.configurations.cpp = {
-  -- {
-    -- name = "Launch",
-    -- type = "codelldb",
-    -- request = "launch",
-    -- program = function()
-      -- return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    -- end,
-    -- cwd = '${workspaceFolder}',
-    -- stopOnEntry = false,
-    -- args = {},
-  -- },
--- }
 
 -- Automatically open and close dap-ui on dap events
 dap.listeners.after.event_initialized["dapui_config"] = function()
