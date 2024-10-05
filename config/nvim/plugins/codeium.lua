a0d7e32c280be37de5361e62e5e2fae8690368cd
laraeter <git28282@gmail.com>
2024-10-05 22:46:17 +0300

    lualine

--- config/nvim/plugins/codeium.lua
+++
@@ -1,8 +0,0 @@
-require("indent-blankline").setup {
-    char = '│', -- Символ для вертикальной линии
-    show_trailing_blankline_indent = false, -- Не показывать отступы для пустых строк
-    show_first_indent_level = true, -- Показать линию на первом уровне отступа
-    use_treesitter = true, -- Использовать Treesitter для точного определения уровней отступов
-    show_current_context = true, -- Показать текущий контекст (например, функцию, в которой находишься)
-    show_current_context_start = true, -- Показывать начало текущего контекста
-}
