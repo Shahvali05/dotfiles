50b90b574d3192e5e82167089c4a78e45e3d0f18
laraeter <git28282@gmail.com>
2024-10-05 13:12:43 +0300

    gopher delete

--- config/nvim/neovim.nix
+++ config/nvim/neovim.nix
@@ -96,7 +96,6 @@
         ${builtins.readFile ./plugins/cokeline.lua}
         ${builtins.readFile ./plugins/comment.lua}
         ${builtins.readFile ./plugins/formatter.lua}
-        ${builtins.readFile ./plugins/gopher.lua}
         ${builtins.readFile ./plugins/lsp.lua}
         ${builtins.readFile ./plugins/lualine.lua}
         ${builtins.readFile ./plugins/noice.lua}
