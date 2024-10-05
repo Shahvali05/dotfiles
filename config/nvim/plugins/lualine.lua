c83cb04965cb22d51a0a06bd132d472f2691c1c7
laraeter <git28282@gmail.com>
2024-10-05 22:39:10 +0300

    lualine

--- config/nvim/plugins/lualine.lua
+++ config/nvim/plugins/lualine.lua
@@ -1,11 +1,4 @@
 require("lualine").setup({
-  options = {
-    theme = 'nord',
-    icons_enabled = true,
-    section_separators = { left = '', right = ''},
-    component_separators = { left = '', right = ''},
-    disabled_filetypes = {},
-  },
   icons_enabled = true,
-  extensions = {'fugitive', 'nvim-tree'}
+  extensions = {'nvim-tree'}
 })
