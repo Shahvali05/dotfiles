142c47ae9ae3cc378294fb9d2fbb9d00e091282b
laraeter <git28282@gmail.com>
2024-10-06 23:55:32 +0300

    nvim gopher

--- config/nvim/plugins/codeium.lua
+++ config/nvim/plugins/codeium.lua
@@ -42,3 +42,40 @@ vim.api.nvim_set_keymap(
   "<Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>",
   { noremap = true, silent = true, expr = false }
 )
+
+local refactoring = require("refactoring")
+local telescope = require("telescope")
+local telescope_actions = require("telescope.actions")
+
+telescope.setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = telescope_actions.close
+      },
+    },
+  },
+}
+
+-- Настроим Telescope для вызова рефакторингов
+local function refactoring_menu()
+  local opts = {
+    layout_strategy = "vertical",
+    sorting_strategy = "ascending",
+    prompt_title = "Select Refactoring Action",
+    layout_config = {
+      width = 0.4,
+      height = 0.5,
+    },
+  }
+
+  telescope.extensions.refactoring.refactors(opts)
+end
+
+-- Установка горячей клавиши для открытия меню выбора действий
+vim.api.nvim_set_keymap(
+  "v", 
+  "<Space>r", 
+  "<Esc><Cmd>lua refactoring_menu()<CR>", 
+  { noremap = true, silent = true, expr = false }
+)
