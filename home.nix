ae2bd3a8a2cf73471b795b538a21d7061bb6066f
laraeter <git28282@gmail.com>
2024-10-06 10:12:10 +0300

    settings fonts

--- home.nix
+++ home.nix
@@ -75,6 +75,12 @@
     # EDITOR = "emacs";
   };
 
+  programs.kitty = {
+    enable = true;
+    font.name = "FiraCode Nerd Font";
+    font.size = 16;
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
