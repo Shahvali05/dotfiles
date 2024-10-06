2ac1cd762dddd0eb6346517f89b02c46c30f8163
laraeter <git28282@gmail.com>
2024-10-06 21:15:51 +0300

    nvim lsp-inlayhints-nvim

--- config/nvim/plugins/codeium.lua
+++ config/nvim/plugins/codeium.lua
@@ -1,1 +1,19 @@
-
+require('lsp-inlayhints').setup({
+  -- Общие настройки
+  inlay_hints = {
+    parameter_hints = {
+      show = true, -- Показывать подсказки для параметров функции
+      prefix = "<- ", -- Префикс для параметров
+    },
+    type_hints = {
+      show = true, -- Показывать подсказки для типов
+      prefix = "", -- Префикс для типа
+      separator = ", ", -- Разделитель для типов
+    },
+    only_current_line = false, -- Показывать подсказки только для текущей строки
+    labels_separator = "  ", -- Разделитель между подсказками
+    highlight = "Comment", -- Группа подсветки для подсказок
+  },
+  -- Автоматическое включение подсказок для всех LSP серверов
+  enabled_at_startup = true,
+})
