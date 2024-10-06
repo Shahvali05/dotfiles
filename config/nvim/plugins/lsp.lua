b910374525ff24ad3c8af02b572ee3294d605ecd
laraeter <git28282@gmail.com>
2024-10-06 22:44:01 +0300

    nvim gopher

--- config/nvim/plugins/lsp.lua
+++ config/nvim/plugins/lsp.lua
@@ -1,90 +1,29 @@
-local lspconfig = require("lspconfig")
+-- Установка плагина
+require('lsp-endhints').setup({
+  inlay_hints = {
+    parameter_hints = true,  -- Включить подсказки для параметров
+    type_hints = true,       -- Включить подсказки типов
+    other_hints = true,      -- Включить прочие подсказки
+    only_current_line = false,  -- Подсказки будут отображаться во всех строках, а не только в текущей
+  },
+  -- Использовать под капотом тот же LSP клиент, что и вы настроили
+  enabled_clients = { 'pyright', 'nil_ls', 'marksman', 'rust_analyzer', 'yamlls', 'bashls', 'clangd', 'gopls' },
+})
 
--- default settings
-require("lsp-endhints").setup {
-	icons = {
-		type = "󰜁 ",
-		parameter = "󰏪 ",
-		offspec = " ", -- hint kind not defined in official LSP spec
-		unknown = " ", -- hint kind is nil
-	},
-	label = {
-		padding = 1,
-		marginLeft = 0,
-		bracketedParameters = true,
-	},
-	autoEnableHints = true,
-}
+-- Включить подсказки при старте для каждого LSP-сервера
+local function on_attach(client)
+  if client.server_capabilities.inlayHintProvider then
+    require('lsp-endhints').on_attach(client)
+  end
+end
 
--- inlay hints will show at the end of the line (default)
-require("lsp-endhints").enable()
+-- Настройка LSP с on_attach
+require'lspconfig'.pyright.setup{ on_attach = on_attach }
+require'lspconfig'.nil_ls.setup{ on_attach = on_attach }
+require'lspconfig'.marksman.setup{ on_attach = on_attach }
+require'lspconfig'.rust_analyzer.setup{ on_attach = on_attach }
+require'lspconfig'.yamlls.setup{ on_attach = on_attach }
+require'lspconfig'.bashls.setup{ on_attach = on_attach }
+require'lspconfig'.clangd.setup{ on_attach = on_attach }
+require'lspconfig'.gopls.setup{ on_attach = on_attach }
 
--- inlay hints will show as if the plugin was not installed
-require("lsp-endhints").disable()
-
--- toggle between the two
-require("lsp-endhints").toggle()
-
--- lua-ls
-require("lspconfig").lua_ls.setup {
-	settings = {
-		Lua = {
-			hint = { enable = true },
-		},
-	},
-}
-
--- tsserver
-local inlayHints = {
-	includeInlayParameterNameHints = "all",
-	includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-	includeInlayFunctionParameterTypeHints = true,
-	includeInlayVariableTypeHints = true,
-	includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-	includeInlayPropertyDeclarationTypeHints = true,
-	includeInlayFunctionLikeReturnTypeHints = true,
-	includeInlayEnumMemberValueHints = true,
-}
-
-require'lspconfig'.pyright.setup{}
-require'lspconfig'.nil_ls.setup{}
-require'lspconfig'.marksman.setup{}
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.yamlls.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.tsserver.setup {
-	settings = {
-		typescript = {
-			inlayHints = inlayHints,
-		},
-		javascript = {
-			inlayHints = inlayHints,
-		},
-	},
-}
-require'lspconfig'.clangd.setup{
-  settings = {
-		clangd = {
-			InlayHints = {
-				Designators = true,
-				Enabled = true,
-				ParameterNames = true,
-				DeducedTypes = true,
-			},
-			fallbackFlags = { "-std=c++20" },
-		},
-	},
-}
-require'lspconfig'.gopls.setup{
-  settings = {
-		hints = {
-			rangeVariableTypes = true,
-			parameterNames = true,
-			constantValues = true,
-			assignVariableTypes = true,
-			compositeLiteralFields = true,
-			compositeLiteralTypes = true,
-			functionTypeParameters = true,
-		},
-	},
-}
