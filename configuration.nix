63ea33a8ebb16c8f4fce412c87d33f27e29b8be6
laraeter <git28282@gmail.com>
2024-12-15 20:18:40 +0300

    wayland enables

--- configuration.nix
+++ configuration.nix
@@ -323,10 +323,10 @@
   services.waydroid.enable = true;
 
   # Обеспечение работы OpenGL (графический рендеринг)
-  hardware.opengl.enable = true;
+  # hardware.opengl.enable = true;
 
   # Подключение модулей ядра, необходимых для Waydroid
-  # boot.kernelModules = [ "binder_linux" "ashmem_linux" ];
+  boot.kernelModules = [ "binder_linux" "ashmem_linux" ];
 
   # Включение поддержки cgroups v1 и v2
   boot.kernelParams = [
