caf32abd815d0585ab8589cad16ab18b29b639b3
laraeter <git28282@gmail.com>
2024-12-25 10:35:49 +0300

    nvim: install mypy

--- config/nvim/plugins/lsp.lua
+++ config/nvim/plugins/lsp.lua
@@ -1,13 +1,7 @@
 local lspconfig = require("lspconfig")
 
 -- require'lspconfig'.pyright.setup{}
-require'lspconfig'.nil_ls.setup({
-  sources = {
-    null_ls.builtins.diagnostics.mypy.with({
-      extra_args = {"--ignore-missing-imports"}, -- Опционально
-    }),
-  },
-})
+require'lspconfig'.nil_ls.setup{}
 require'lspconfig'.marksman.setup{}
 require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.yamlls.setup{}
@@ -15,3 +9,15 @@ require'lspconfig'.bashls.setup{}
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.gopls.setup{}
 -- require'lspconfig'.ccls.setup{}
+
+-- Подключение null-ls
+local null_ls = require("null-ls")
+
+-- Настройка null-ls для использования mypy
+null_ls.setup({
+  sources = {
+    null_ls.builtins.diagnostics.mypy.with({
+      extra_args = {"--ignore-missing-imports"}, -- Опционально
+    }),
+  },
+})
