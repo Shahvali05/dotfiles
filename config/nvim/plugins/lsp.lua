7de11d069bb36281fe85ba995c5aa6623b467722
laraeter <git28282@gmail.com>
2024-12-25 11:08:29 +0300

    nvim: install jedi

--- config/nvim/plugins/lsp.lua
+++ config/nvim/plugins/lsp.lua
@@ -12,16 +12,16 @@ require'lspconfig'.gopls.setup{}
 -- require'lspconfig'.ccls.setup{}
 
 -- Включаем диагностику, но отключаем обновление в режиме вставки
--- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  -- vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- virtual_text = true,        -- Включаем виртуальный текст
-    -- signs = true,               -- Используем знаки для ошибок
-    -- update_in_insert = true,   -- Отключаем обновление ошибок в режиме вставки
-  -- }
--- )
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true,        -- Включаем виртуальный текст
+    signs = true,               -- Используем знаки для ошибок
+    update_in_insert = true,   -- Отключаем обновление ошибок в режиме вставки
+  }
+)
 
 -- Дополнительно для улучшенной подсветки ошибок
--- vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float({ focusable = false })]])
+vim.cmd([[autocmd CursorHold * lua vim.diagnostic.open_float({ focusable = false })]])
 
 -- Подключение null-ls
 local null_ls = require("null-ls")
