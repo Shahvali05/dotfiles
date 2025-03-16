a0363b6cb2c29717d9d5d7151f5128683d20c55d
Shahvali Magomedov <msahvali@gmail.com>
2025-03-16 15:22:15 +0300

    nvim: update config

--- config/nvim/plugins/lualine.lua
+++ config/nvim/plugins/lualine.lua
@@ -1,3 +1,19 @@
-require("lualine").setup({
-  icons_enabled = true,
-})
+require('lualine').setup {
+  options = {
+    theme = 'auto', -- или ваша тема
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+  tabline = {
+    lualine_a = { 'buffers' }, -- Отображение буферов в верхней строке
+    lualine_z = { 'tabs' },    -- Опционально: вкладки справа
+  },
+}
