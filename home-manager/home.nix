1696644d8a1c01e3264e269e434cd77687df3d5b
Shahvali Magomedov <msahvali@gmail.com>
2025-04-20 12:17:38 +0300

    home-manager: mako - settings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,26 +18,26 @@
   home.file = {
   };
 
-  services.mako = {
-    enable = true;
-    anchor = "top-right";
-    defaultTimeout = 3000;
-    backgroundColor = "#2e344099";
-    textColor = "#d8dee9";
-    borderColor = "#4c566a";
-    borderSize = 2;
-    borderRadius = 5;
-    font = "Sans 10";
-    width = 300;
-    height = 100;
-    padding = "10";
-    margin = "10";
-    layer = "overlay";
-    extraConfig = ''
-      [urgency=normal]
-      on-notify=exec mpv ~/.local/share/sounds/notification.wav
-    '';
-  };
+  # services.mako = {
+  #   enable = true;
+  #   anchor = "top-right";
+  #   defaultTimeout = 3000;
+  #   backgroundColor = "#2e344099";
+  #   textColor = "#d8dee9";
+  #   borderColor = "#4c566a";
+  #   borderSize = 2;
+  #   borderRadius = 5;
+  #   font = "Sans 10";
+  #   width = 300;
+  #   height = 100;
+  #   padding = "10";
+  #   margin = "10";
+  #   layer = "overlay";
+  #   extraConfig = ''
+  #     [urgency=normal]
+  #     on-notify=exec mpv ~/.local/share/sounds/notification.wav
+  #   '';
+  # };
 
   home.sessionVariables = {
     XDG_CURRENT_DESKTOP = "Hyprland";
