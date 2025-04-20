5a2e25419f8d08fcebe44d7ffc4fd2925ff64149
Shahvali Magomedov <msahvali@gmail.com>
2025-04-20 06:19:31 +0300

    home-manager: neovim - disable formater

--- nix-config/home-manager/nvim/plugins/formatter.lua
+++ nix-config/home-manager/nvim/plugins/formatter.lua
@@ -1,4 +1,4 @@
-local formatter = require("formatter")
+-- local formatter = require("formatter")
 
 
 -- ============================================================================
@@ -6,41 +6,41 @@ local formatter = require("formatter")
 -- ============================================================================
 
 
-formatter.setup({
-  logging = false,
-  filetype = {
+-- formatter.setup({
+--   logging = false,
+--   filetype = {
 
-    python = {
-      function()
-        return {
-          exe   = "black",
-          args  = { "--quiet", "-" },
-          stdin = true,
-        }
-      end,
-    },
+--     python = {
+--       function()
+--         return {
+--           exe   = "black",
+--           args  = { "--quiet", "-" },
+--           stdin = true,
+--         }
+--       end,
+--     },
 
-    cpp = {
-      function()
-        return {
-          exe   = "clang-format",
-          args  = { "--style=file", "-" },
-          stdin = true,
-        }
-      end,
-    },
+--     cpp = {
+--       function()
+--         return {
+--           exe   = "clang-format",
+--           args  = { "--style=file", "-" },
+--           stdin = true,
+--         }
+--       end,
+--     },
 
-    go = {
-      function()
-        return {
-          exe   = "gofmt",
-          args  = {},
-          stdin = true,
-        }
-      end,
-    },
-  },
-})
+--     go = {
+--       function()
+--         return {
+--           exe   = "gofmt",
+--           args  = {},
+--           stdin = true,
+--         }
+--       end,
+--     },
+--   },
+-- })
 
 
 -- ============================================================================
