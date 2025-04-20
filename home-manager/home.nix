5196f8f136daeac8f1eb94f93a0ab0ad070d2e5a
Shahvali Magomedov <msahvali@gmail.com>
2025-04-20 12:15:21 +0300

    home-manager: mako - settings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -20,24 +20,22 @@
 
   services.mako = {
     enable = true;
-    # Основные параметры
-    anchor = "top-right"; # Уведомления в правом верхнем углу
-    defaultTimeout = 3000; # 3 секунды
-    backgroundColor = "#2e344099"; # Серо-синий с прозрачностью 80% (99 в hex = ~0.6 alpha)
-    textColor = "#d8dee9"; # Светлый текст для контраста
-    borderColor = "#4c566a"; # Граница чуть темнее
-    borderSize = 2; # Толщина границы
-    borderRadius = 5; # Скругленные углы для красоты
-    font = "Sans 10"; # Минималистичный шрифт
-    width = 300; # Ширина уведомления
-    height = 100; # Высота уведомления
-    padding = "10"; # Внутренние отступы
-    margin = "10"; # Внешние отступы от края экрана
-    layer = "overlay"; # Уведомления поверх всего
-    # Звуковая настройка
+    anchor = "top-right";
+    defaultTimeout = 3000;
+    backgroundColor = "#2e344099";
+    textColor = "#d8dee9";
+    borderColor = "#4c566a";
+    borderSize = 2;
+    borderRadius = 5;
+    font = "Sans 10";
+    width = 300;
+    height = 100;
+    padding = "10";
+    margin = "10";
+    layer = "overlay";
     extraConfig = ''
       [urgency=normal]
-      on-notify=exec mpv ${pkgs.sound-theme-elementary}/share/sounds/elementary/stereo/message-new-instant.wav
+      on-notify=exec mpv ~/.local/share/sounds/notification.wav
     '';
   };
 
