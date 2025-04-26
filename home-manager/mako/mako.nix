d93c2a4aae54358dda1c77087b61692839d31445
Shahvali Magomedov <msahvali@gmail.com>
2025-04-26 15:34:37 +0300

    home-manager: settings mako

--- home-manager/mako/mako.nix
+++ home-manager/mako/mako.nix
@@ -3,15 +3,11 @@
 {
   programs.mako = {
     enable = true;  # включает mako (устанавливает пакет и запускает daemon)
-    settings = {
-      anchor = "top-right";    # позиционирование уведомлений
-      border-radius = 8;        # радиус скругления углов
-
-      background-color = "#445577CC";  # цвет фона (CC = ~0.8 прозрачности)
-      text-color       = "#FFFFFFFF";  # цвет текста (здесь — белый непрозрачный)
-
-      default-timeout  = 5000;   # время (мс) показа уведомления, 0 = бесконечно
-      ignore-timeout   = false;  # использовать указанный default-timeout
-    };
+    anchor = "top-right";    # позиционирование уведомлений
+    borderRadius = 8;        # радиус скругления углов
+    backgroundColor = "#445577CC";  # цвет фона (CC = ~0.8 прозрачности)
+    textColor = "#FFFFFFFF";  # цвет текста (здесь — белый непрозрачный)
+    defaultTimeout = 5000;   # время (мс) показа уведомления, 0 = бесконечно
+    ignoreTimeout = false;  # использовать указанный default-timeout
   };
 }
