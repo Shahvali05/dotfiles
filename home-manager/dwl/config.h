2079388e527624e41c52d27aff2625df5730f9c8
Shahvali Magomedov <msahvali@gmail.com>
2025-04-27 16:19:35 +0300

    home-manager: install dwl

--- home-manager/dwl/config.h
+++ home-manager/dwl/config.h
@@ -54,6 +54,9 @@ static XkbRules xkb_rules = {
 static const int repeat_rate = 25;
 static const int repeat_delay = 600;
 
+/* logging */
+static const enum wlr_log_importance log_level = WLR_DEBUG; /* Добавлено для log_level */
+
 /* trackpad */
 static LibinputConfigTrackpad trackpad_config = {
     .tap_to_click = 1,
@@ -67,7 +70,7 @@ static LibinputConfigTrackpad trackpad_config = {
     .click_method = LIBINPUT_CONFIG_CLICK_METHOD_BUTTON_AREAS,
     .send_events_mode = LIBINPUT_CONFIG_SEND_EVENTS_ENABLED,
     .accel_profile = LIBINPUT_CONFIG_ACCEL_PROFILE_ADAPTIVE,
-    .accel_speed = 0.0,
+    .accel_speed = 0.0, /* Добавлено для accel_speed */
     .button_map = LIBINPUT_CONFIG_TAP_MAP_LRM,
 };
 
@@ -100,13 +103,9 @@ static const Key keys[] = {
     { MODKEY|WLR_MODIFIER_SHIFT,    XKB_KEY_comma, toggletag,  {.ui = 1 << 7 } },
     { MODKEY,                       XKB_KEY_period, view,      {.ui = 1 << 8 } },
     { MODKEY|WLR_MODIFIER_SHIFT,    XKB_KEY_period, toggletag, {.ui = 1 << 8 } },
-    /* Добавленные привязки для устранения предупреждений */
-    { MODKEY,                       XKB_KEY_f, togglefullscreen, {0} }, /* Полноэкранный режим */
-    { MODKEY,                       XKB_KEY_t, tag,            {.ui = 1 << 0} }, /* Переключение тэга */
-    { MODKEY,                       XKB_KEY_m, tagmon,         {.i = +1} }, /* Переключение монитора */
-    { MODKEY,                       XKB_KEY_c, killclient,     {0} }, /* Закрытие клиента */
-    { MODKEY,                       XKB_KEY_n, focusmon,       {.i = +1} }, /* Фокус на мониторе */
-    { MODKEY,                       XKB_KEY_v, chvt,           {.i = 1} }, /* Смена виртуального терминала */
+    /* Добавлены для устранения предупреждений */
+    { MODKEY,                       XKB_KEY_z, zoom,           {0} }, /* Zoom */
+    { MODKEY,                       XKB_KEY_v, toggleview,     {.ui = 1 << 0} }, /* Toggleview */
     TAGKEYS(                        XKB_KEY_1, XKB_KEY_9,      1, 9 ),
 };
 
