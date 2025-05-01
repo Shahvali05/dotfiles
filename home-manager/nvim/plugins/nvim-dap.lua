6bf79c1c60f028e245b882574ec50678b10cbfd7
Shahvali Magomedov <msahvali@gmail.com>
2025-05-02 02:32:57 +0300

    home-manager: nvim: settings

--- home-manager/nvim/plugins/nvim-dap.lua
+++ home-manager/nvim/plugins/nvim-dap.lua
@@ -123,7 +123,8 @@ end
 -- Горячие клавиши отладки
 -- ============================================================================
 
-vim.api.nvim_set_keymap('n', '<F5>',     ':lua require"dap".continue()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F5>', ':Telescope dap configurations<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', '<F5>',     ':lua require"dap".continue()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F10>',    ':lua require"dap".step_over()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F11>',    ':lua require"dap".step_into()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F12>',    ':lua require"dap".step_out()<CR>',  { noremap = true, silent = true })
