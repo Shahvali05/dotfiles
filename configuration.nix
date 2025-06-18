d2c74cfc33d2764f4338913a893732083de4f5a5
Shahvali Magomedov <msahvali@gmail.com>
2025-06-18 20:29:05 +0300

    enable tftpd-hpa

--- configuration.nix
+++ configuration.nix
@@ -24,4 +24,10 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "24.11";
+
+  services.tftpd-hpa = {
+    enable = true;
+    directories = [ "/var/tftp" ];
+    # port = 69;
+  };
 }
--- system/network.nix
+++ system/network.nix
@@ -20,7 +20,7 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  networking.firewall.allowedTCPPorts = [ 8000 ];
+  networking.firewall.allowedTCPPorts = [ 8000, 69 ];
 
   # networking.interfaces.wlp4s0.ipv4.addresses = [
   #   {address = "192.168.1.2"; prefixLength = 24; }
