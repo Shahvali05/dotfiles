711b134d2028510ee14a49f731901c56d766dd12
laraeter <git28282@gmail.com>
2025-07-18 03:51:16 +0300

    create service tartarus-startpage

--- system/services.nix
+++ system/services.nix
@@ -44,7 +44,7 @@
 
   services.flatpak.enable = true;
 
-  services.indexHtmlServer = {
+  systemd.services.indexHtmlServer = {
     description = "tartarus-startpage";
     after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
