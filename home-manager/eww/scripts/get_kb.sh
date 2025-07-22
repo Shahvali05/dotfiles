d7696677f9b4d984dd521e081dea697523da8510
laraeter <git28282@gmail.com>
2025-07-22 03:15:42 +0300

    home-manager: settings eww

--- home-manager/eww/scripts/get_kb.sh
+++ home-manager/eww/scripts/get_kb.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-# Проверяем, доступен ли hyprctl
 if ! command -v hyprctl &> /dev/null; then
     echo "Ошибка: hyprctl не найден. Убедитесь, что вы используете Hyprland."
     exit 1
 fi
 
-# Получаем текущую раскладку с помощью hyprctl
-CURRENT_LAYOUT=$(hyprctl devices | grep -B 5 "main: yes" | grep "active keymap" | awk '{print $NF}')
+while true; do
+  CURRENT_LAYOUT=$(hyprctl devices | grep -B 5 "main: yes" | grep "active keymap" | awk '{print $NF}')
 
-if [ "$CURRENT_LAYOUT" == "(US)" ]; then
-    CURRENT_LAYOUT="US"
-elif [ "$CURRENT_LAYOUT" == "Russian" ]; then
-    CURRENT_LAYOUT="RU"
-fi
+  if [ "$CURRENT_LAYOUT" == "(US)" ]; then
+      CURRENT_LAYOUT="US"
+  elif [ "$CURRENT_LAYOUT" == "Russian" ]; then
+      CURRENT_LAYOUT="RU"
+  fi
 
-# Проверяем, удалось ли получить раскладку
-if [ -z "$CURRENT_LAYOUT" ]; then
-    echo "Ошибка: Не удалось определить текущую раскладку клавиатуры."
-    exit 1
-fi
+  if [ -z "$CURRENT_LAYOUT" ]; then
+      echo "Ошибка: Не удалось определить текущую раскладку клавиатуры."
+      exit 1
+  fi
 
-# Выводим текущую раскладку
-echo "$CURRENT_LAYOUT"
+  echo "$CURRENT_LAYOUT"
+  sleep 0.1
+done
