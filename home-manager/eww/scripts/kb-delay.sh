ee1168fb129d6248b5f124c1f6d1510567da8991
laraeter <git28282@gmail.com>
2025-07-22 03:02:11 +0300

    home-manager: settings eww

--- home-manager/eww/scripts/kb-delay.sh
+++ home-manager/eww/scripts/kb-delay.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-/usr/bin/eww update keyhov=true
-(sleep 0.45 && /usr/bin/eww update keyrev="$(/usr/bin/eww get keyhov)") &
+eww update keyhov=true
+(sleep 0.45 && eww update keyrev="$(eww get keyhov)") &
