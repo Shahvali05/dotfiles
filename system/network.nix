2ade9db3dc3ac4a0ed6fc3148ddbd0ed84ff99af
laraeter <git28282@gmail.com>
2025-07-29 11:54:35 +0300

    allow udp port 7551 19132

--- system/network.nix
+++ system/network.nix
@@ -21,7 +21,7 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 8000 69 18101 19132 ];
-  # networking.firewall.allowedUDPPorts = [ 19132 7551 ];
+  networking.firewall.allowedUDPPorts = [ 19132 7551 ];
 
   # networking.interfaces.wlp4s0.ipv4.addresses = [
   #   {address = "192.168.1.2"; prefixLength = 24; }
