9ca707f555ca9bc35447791296067c04a22b5a94
laraeter <git28282@gmail.com>
2025-09-04 15:42:23 +0300

    home-manager: settings nvim

--- home-manager/nvim/plugins/nvim-tree.lua
+++ home-manager/nvim/plugins/nvim-tree.lua
@@ -56,6 +56,17 @@ nvimtree.setup({
   },
 })
 
+-- Автоматически закрывать nvim-tree, если он остался единственным окном
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and
+       vim.bo.filetype == "NvimTree" then
+      vim.cmd("quit")
+    end
+  end
+})
+
 -- ============================================================================
 -- Горячие клавиши
 -- ============================================================================
@@ -63,3 +74,5 @@ nvimtree.setup({
 
 local keymap = vim.keymap
 keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Открыть NvimTree" })
+
+
