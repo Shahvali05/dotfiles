421576d995e258e084c9bccea068fc4864705813
laraeter <git28282@gmail.com>
2025-09-25 00:21:17 +0300

    uninstall gnome

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,6 @@
   xdg.portal.enable = true;
   xdg.portal.wlr.enable = true;
 
-  # services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
 }
