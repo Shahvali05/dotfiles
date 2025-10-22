282ac65a48b99c270a840c3e224dd0ebb8e23f23
msahvali <msahvali@gmail.com>
2025-10-22 11:56:05 +0300

    home-manager: dwl: add autosize

--- home-manager/dwl/config.h
+++ home-manager/dwl/config.h
@@ -34,7 +34,7 @@ static int log_level = WLR_ERROR;
 static const char *const autostart[] = {
         // "dbus-daemon", "--session", "--address=unix:path=$XDG_RUNTIME_DIR/bus", "--nofork", NULL,
         "wbg", "/home/red/.wallpapers/pexels-pok-rie-33563-2049422.jpg", NULL,
-        "wlr-randr", "--output", "eDP-1", "--scale", "2", NULL,
+        // "wlr-randr", "--output", "eDP-1", "--scale", "2", NULL,
         NULL /* terminate */
 };
 
