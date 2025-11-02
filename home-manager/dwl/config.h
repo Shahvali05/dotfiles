890b2c2654c6812d58d9f49d79b31ca9139056e6
msahvali <msahvali@gmail.com>
2025-11-02 15:32:07 +0300

    home-manager: dwl: update bing Print

--- home-manager/dwl/config.h
+++ home-manager/dwl/config.h
@@ -207,7 +207,7 @@ static const Key keys[] = {
         { 0,                         0x1008ffb2,         spawn,          {.v = (const char*[]) { "pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL }} },
         { 0,                         0x1008ff03,         spawn,          {.v = (const char*[]) { "brightnessctl", "set", "10%-", NULL }} },
         { 0,                         0x1008ff02,         spawn,          {.v = (const char*[]) { "brightnessctl", "set", "+10%", NULL }} },
-	{ 0,                         0xff61,             spawn,          {.v = (const char*[]) { "sh", "-c", 'grim -g "$(slurp -d)" - | wl-copy', NULL }} },
+	// { 0,                         0xff61,             spawn,          {.v = (const char*[]) { "sh", "-c", 'grim -g "$(slurp -d)" - | wl-copy', NULL }} },
 
 	/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
 	{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
