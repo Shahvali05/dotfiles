33e22fa00fd010940299e9b76f1b173b5602a0ad
msahvali <msahvali@gmail.com>
2025-11-06 07:58:10 +0300

    home-manager: neovim: settings

--- home-manager/nvim/plugins/refactoring.lua
+++ home-manager/nvim/plugins/refactoring.lua
@@ -25,37 +25,3 @@ require('refactoring').setup({
   printf_statements     = {}, -- Настройки для printf-выражений (оставлено пустым)
   print_var_statements  = {}, -- Настройки для отладки переменных (оставлено пустым)
 })
-
-
--- ============================================================================
--- Привязки клавиш для визуального режима (рефакторинг)
--- ============================================================================
-
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>re",
-  "<Esc><Cmd>lua require('refactoring').refactor('extract function')<CR>",
-  { noremap = true, silent = true, expr = false, desc = "Извлечь функцию из выбранного кода" }
-)
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>rf",
-  "<Esc><Cmd>lua require('refactoring').refactor('extract_to_file')<CR>",
-  { noremap = true, silent = true, expr = false, desc = "Извлечь функцию в отдельный файл" }
-)
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>rv",
-  "<Esc><Cmd>lua require('refactoring').refactor('extract_var')<CR>",
-  { noremap = true, silent = true, expr = false, desc = "Извлечь переменную из выражения" }
-)
-
-vim.api.nvim_set_keymap(
-  "v",
-  "<leader>ri",
-  "<Esc><Cmd>lua require('refactoring').refactor('inline_var')<CR>",
-  { noremap = true, silent = true, expr = false, desc = "Вставить переменную напрямую" }
-)
